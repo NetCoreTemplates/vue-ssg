c2b8c7f4d3ff3e60cb6cf6affb1fcc0437204fc4
Demis Bellot <demis.bellot@gmail.com>
2021-11-30 14:02:45 +0800

    Replace Todos to use Functional Component instead of class var

--- ui/src/pages/Todos.vue
+++ ui/src/pages/Todos.vue
@@ -1,11 +1,9 @@
 <template>
-  <div class="min-h-screen">
-    <main class="flex justify-center">
-      <div class="mx-auto px-5 mb-32">
-        <AppBreadcrumb class="my-8" name="Todos Application"/>
+  <AppPage title="Todos Application" class="max-w-prose">
 
-        <input type="text" v-model="store.newTodo" placeholder="What needs to be done?" @keyup.enter="store.addTodo()"
-               class="w-full shadow mb-4" />
+        <input type="text" v-model="store.newTodo" placeholder="What needs to be done?" 
+               class="w-full shadow mb-4"
+               @keyup.enter="store.addTodo()">
 
         <div class="bg-white shadow overflow-hidden rounded-md">
           <ul role="list" class="divide-y divide-gray-200">
@@ -30,42 +28,48 @@
           <div class="text-gray-400 leading-8 mr-4">
             {{ store.unfinishedTodos.length }} item(s) left
           </div>
-          <div>
-            <span class="relative z-0 inline-flex shadow-sm rounded-md">
-              <button type="button" :class="`${btnCls} rounded-l-md relative ${btnActiveCls(store.filter === 'all')}`" @click="store.filter='all'">
-                All
-              </button>
-              <button type="button" :class="`${btnCls} -ml-px relative ${btnActiveCls(store.filter === 'unfinished')}`" @click="store.filter='unfinished'">
-                Active
-              </button>
-              <button type="button" :class="`${btnCls} rounded-r-md -ml-px ${btnActiveCls(store.filter === 'finished')}`" @click="store.filter='finished'">
-                Completed
-              </button>
-            </span>            
+
+          <div class="inline-flex shadow-sm rounded-md">
+            <FilterTab class="rounded-l-lg border" filter="all">
+              Profile
+            </FilterTab>
+            <FilterTab class="border-t border-b" filter="unfinished">
+              Active
+            </FilterTab>
+            <FilterTab class="rounded-r-md border" filter="finished">
+              Completed
+            </FilterTab>
           </div>
+          
           <div class="leading-8 ml-4">
-            <a href="#" :class="{ invisible:store.finishedTodos.length === 0 }" @click.prevent="store.removeFinishedTodos()">
+            <a href="#" v-if="store.finishedTodos.length" @click.prevent="store.removeFinishedTodos()">
               clear completed
             </a>
           </div>
         </div>
 
-        <article class="prose lg:prose-xl">
-        </article>
-
-      </div>
-    </main>
-  </div>
-
+  </AppPage>
 </template>
 
 <script setup lang="ts">
-import { useTodosStore } from "@/stores/todos"
-import AppBreadcrumb from "@/components/AppBreadcrumb.vue"
+import { h, SetupContext } from "vue"
+import { Filter, useTodosStore } from "@/stores/todos"
+import AppPage from "@/components/AppPage.vue"
 
 const store = useTodosStore()
 
-const btnCls = 'relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:z-10 focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500'
-const btnActiveCls = (isActive:boolean) => isActive ? 'bg-blue-50 hover:bg-blue-50' : '' 
-
+// Should be in own FilterTab.Vue SFC, using Functional Component to retain in single file
+const FilterTab = (props:{ filter:Filter }, context:SetupContext) => {
+  return h('a', {
+    href: '#',
+    'class': `border-gray-200 text-sm font-medium px-4 py-2 hover:bg-gray-100 
+      focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:border-gray-600 
+      dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white
+      ${props.filter == store.filter ? 'text-blue-700 dark:bg-blue-600' : 'text-gray-900 hover:text-blue-700 dark:bg-gray-700'}`,
+    onClick: (e:MouseEvent) => {
+      e.preventDefault()
+      store.changeFilter(props.filter)
+    }
+  }, context.slots);
+}
 </script>
--- ui/src/stores/todos.ts
+++ ui/src/stores/todos.ts
@@ -1,16 +1,17 @@
 import { acceptHMRUpdate, defineStore } from 'pinia'
 
-type Todo = {
+export type Todo = {
     id: number
     text: string
     isFinished: boolean
 }
+export type Filter = "all" | "finished" | "unfinished"
 
 export const useTodosStore = defineStore('todos', () => {
     // State
     const todos = ref<Todo[]>([])
     const newTodo = ref("")
-    const filter = ref<"all" | "finished" | "unfinished">('all')
+    const filter = ref<Filter>('all')
     const nextId = ref(0)
 
     // Getters
@@ -38,6 +39,7 @@ export const useTodosStore = defineStore('todos', () => {
     }
     const removeTodo = (id: number) => { todos.value = todos.value.filter(x => x.id != id) }
     const removeFinishedTodos = () => { todos.value = todos.value.filter(x => !x.isFinished) }
+    const changeFilter = (value:Filter) => filter.value = value
 
     return {
         newTodo,
@@ -49,6 +51,7 @@ export const useTodosStore = defineStore('todos', () => {
         toggleTodo,
         removeTodo,
         removeFinishedTodos,
+        changeFilter,
     }
 })
 
