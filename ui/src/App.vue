bc381f780840221564142cd43118bb575535890e
Demis Bellot <demis.bellot@gmail.com>
2021-12-23 15:58:32 +0800

    Add support for QueryBookings

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { useAppStore } from "@/stores/app"
+
 // https://github.com/vueuse/head
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
@@ -8,6 +10,9 @@ useHead({
     { name: 'description', content: 'Vite SSG Starter Template' },
   ],
 })
+
+let store = useAppStore()
+store.load()
 </script>
 
 <template>
