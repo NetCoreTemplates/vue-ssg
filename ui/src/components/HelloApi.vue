e9e8931f647817ce18f818bf70169befcdee76bc
Demis Bellot <demis.bellot@gmail.com>
2023-05-24 10:49:46 +0800

    Replace swrv with built-in client.swr

--- ui/src/components/HelloApi.vue
+++ ui/src/components/HelloApi.vue
@@ -1,13 +1,20 @@
 <template>
-  <div v-if="error" class="ml-2 text-red-500">{{ error.message }}</div>
-  <div v-else class="ml-3 mt-2 text-2xl">{{ data ? data.result : 'loading...' }}</div>
+  <div v-if="api.error" class="ml-2 text-red-500">{{ api.error!.message }}</div>
+  <div v-else class="ml-3 mt-2 text-2xl">{{ api.response ? api.response.result : 'loading...' }}</div>
 </template>
 
 <script setup lang="ts">
-import { Hello } from "@/dtos"
-import { swrClient } from "@/api"
+import { ref } from "vue"
+import { Hello, HelloResponse } from "@/dtos"
+import { ApiResult } from "@servicestack/client"
+import { useClient } from "@servicestack/vue"
+
+const client = useClient()
 
 const props = defineProps<{ name: string }>()
+const api = ref(new ApiResult<HelloResponse>())
 
-const { data, error } = swrClient.get(() => new Hello({ name: props.name }))
+const update = () => client.swr(new Hello({ name: props.name }), r => api.value = r) 
+watch([props], update)
+update()
 </script>
